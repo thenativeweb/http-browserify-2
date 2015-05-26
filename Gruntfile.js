45bd6143e51fc3fcf3f882ba1de5d125af8badf1
Golo Roden <webmaster@goloroden.de>
2015-05-26 16:02:00 +0200

    Added test-browser tasl.

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,5 +13,8 @@ module.exports = tourism({
   },
   test: {
     server: [ 'test/**/*.js' ]
+  },
+  shell: {
+    "test-browser": "zuul --ui mocha-tdd --local 8080 --open -- test/httpTests.js"
   }
 });
